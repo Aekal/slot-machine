448a70e5759bffa79e3770eed8c04ddcd9b36b66
Damian Fiałkiewicz <aekal4@gmail.com>
2016-10-22 09:10:43 +0200

    Files structure added

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        notify: false,
+        server: "./"
+    });
+
+    gulp.watch("scss/style.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("js/*.js").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/style.scss")
+        .pipe(sass().on('error', function(err) { //Compile and compress sass
+            console.error(err.message);
+            browserSync.notify(err.message, 3000); // Display error
+            this.emit('end'); // Prevent gulp from catching the error and exiting the watch process
+        }))
+        .pipe(gulp.dest("./css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
